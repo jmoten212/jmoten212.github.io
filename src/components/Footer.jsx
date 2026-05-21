86bd69980fc233c7ecb38f043a0d78763897bcbd
James <53585345+jmoten212@users.noreply.github.com>
2026-05-21 18:34:22 -0400

    Adjustments to footer / modal title spacing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <p>© All content, trademarks, logos, and intellectual property displayed in these videos and images is the exclusive property of Everfi or their respective owners. These videos and images are merely being used as examples to display my previous work at the company.</p>
+    <p>© All content, trademarks, logos, and intellectual property displayed in these Prior Work Project videos and images is the exclusive property of Everfi or their respective owners. These videos and images are merely being used as examples to display my previous work at the company.</p>
   )
 }
 
